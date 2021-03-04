b6751579b07ac95a48c9601712b7245dd72d8321
cebrail <cebrailcl@gmail.com>
2021-03-05 01:49:20 +0300

    centralized-configuration with config server

--- payment-service/src/main/java/com/cbrl/cloud/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/cbrl/cloud/payment/PaymentServiceApplication.java
@@ -2,12 +2,8 @@ package com.cbrl.cloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,17 +11,7 @@ import java.util.Arrays;
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ctx =SpringApplication.run(PaymentServiceApplication.class, args);
-
-
-
-        System.out.println("Inspecting the beans");
-
-        String[] beans = ctx.getBeanDefinitionNames();
-        Arrays.sort(beans);
-        for (String name : beans) {
-            System.out.println("Bean Name" +name);
-        }
+        SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
 }
--- user-service/src/main/java/com/cbrl/spring/cloud/user/UserServiceApplication.java
+++ user-service/src/main/java/com/cbrl/spring/cloud/user/UserServiceApplication.java
@@ -2,12 +2,9 @@ package com.cbrl.spring.cloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 public class UserServiceApplication {
 
